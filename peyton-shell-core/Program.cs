fdf413347b773c56fd53b769df221fedf25ee6a6
Zebuus-afk <83792806+Zebuus-afk@users.noreply.github.com>
2021-05-07 01:32:36 +0200

    Add files via upload

---
+++ peyton-shell-core/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        /* ----- VARIABLES ----- */
+        bool a = true;
+        string command;
+
+
+        /* ----- PRESENTATION ----- */
+        Console.WriteLine("PEYTON SHELL - v.b.0.1");
+        Console.WriteLine("||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||");
+        Console.WriteLine("This program is a simple shell like PowerShell.");
+        Console.WriteLine("So, if you use this shell, you don't will become a hacker (lol).");
+        Console.WriteLine("Press a key for get access to program.");
+        Console.ReadKey();
+        /* ----- MAKE A PAUSE ----- */
+        Console.WriteLine("Oups, this program is in development...");
+        while (a == true)
+        {
+            Console.Write(">>>");
+            command = Console.ReadLine();
+            if (command == "exit")
+            {
+                a = false;
+                continue;
+            }
+            else
+            {
+                Console.WriteLine(command);
+            }
+        }
+    }
+}
